7ef1e1a0e88ae0df30aa30ba78acc7680d442dc2
farias09 <f.ariasartavia09@gmail.com>
2024-10-06 21:07:24 -0600

    Services completed

---
+++ Northwind202403/Backend/Services/Implementations/CategoryService.cs
@@ -0,0 +1,80 @@
+﻿using Backend.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace Backend.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        IUnidadDeTrabajo Unidad;
+
+        public CategoryService(IUnidadDeTrabajo unidadDeTrabajo)
+        {
+            this.Unidad = unidadDeTrabajo;
+        }
+        public bool Agregar(Category category)
+        {
+            try
+            {
+                Unidad.CategoryDAL.Add(category);
+                return Unidad.complete();
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Editar(Category category)
+        {
+            try
+            {
+                Unidad.CategoryDAL.Update(category);
+                return Unidad.complete();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(Category category)
+        {
+            try
+            {
+                Unidad.CategoryDAL.Remove(category);
+                return Unidad.complete();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Category Obtener(int id)
+        {
+            try
+            {
+                return Unidad.CategoryDAL.Get(id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Category> Obtener()
+        {
+            try
+            {
+                return Unidad.CategoryDAL.GetAll().ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Northwind202403/Backend/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Entities;
+
+namespace Backend.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        bool Agregar(Category category);
+        bool Editar (Category category);
+        bool Eliminar (Category category);
+
+        /// <summary>
+        /// Obtiene Category por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Category Obtener(int id);
+
+        /// <summary>
+        /// Obtiene todas las categories
+        /// </summary>
+        /// <returns></returns>
+        List<Category> Obtener();
+    }
+}

466f5352df7dce16675c9f6d25213b800d6b0e1f
farias09 <f.ariasartavia09@gmail.com>
2024-10-06 21:24:52 -0600

    Dependencies Injection

---
+++ Northwind202403/Backend/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using Backend.Services.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return categoryService.Obtener();
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Northwind202403/Backend/Program.cs
+++ Northwind202403/Backend/Program.cs
@@ -1,3 +1,9 @@
+using Backend.Services.Implementations;
+using Backend.Services.Interfaces;
+using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region DI
+builder.Services.AddDbContext<NorthwindContext>();
+builder.Services.AddScoped<IUnidadDeTrabajo, UnidadDeTrabajo>();
+builder.Services.AddScoped<ICategoryDAL, CategoryDALImpl>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+#endregion
 
 var app = builder.Build();
 

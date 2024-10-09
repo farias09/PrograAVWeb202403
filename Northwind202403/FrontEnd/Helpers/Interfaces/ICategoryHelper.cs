bcdc1168f61fbef870a4d346cf1e8918026e9c34
farias09 <f.ariasartavia09@gmail.com>
2024-10-08 22:48:33 -0600

    delete completed

--- Northwind202403/FrontEnd/Helpers/Interfaces/ICategoryHelper.cs
+++ Northwind202403/FrontEnd/Helpers/Interfaces/ICategoryHelper.cs
@@ -8,6 +8,6 @@ namespace FrontEnd.Helpers.Interfaces
         CategoryViewModel GetCategory(int id);
         CategoryViewModel Add(CategoryViewModel category);
         CategoryViewModel Update(CategoryViewModel category);
-        CategoryViewModel Delete(int id);
+        void Delete(int id);
     }
 }

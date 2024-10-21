21c5f24e3233d881077c37aa0d6da5a2ea159749
farias09 <f.ariasartavia09@gmail.com>
2024-10-21 14:48:34 -0600

    ProductHelper

---
+++ Northwind202403/FrontEnd/Helpers/Interfaces/IProductHelper.cs
@@ -0,0 +1,13 @@
+﻿using FrontEnd.Models;
+
+namespace FrontEnd.Helpers.Interfaces
+{
+    public interface IProductHelper
+    {
+        List<ProductViewModel> GetAll();
+        ProductViewModel GetById(int id);
+        ProductViewModel AddProduct(ProductViewModel product);
+        ProductViewModel EditProduct(ProductViewModel product);
+        void DeleteProduct(int id);
+    }
+}
---
+++ Northwind202403/FrontEnd/Models/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontEnd.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public int? SupplierId { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+}

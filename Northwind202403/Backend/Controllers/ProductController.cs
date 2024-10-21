2fd6c9ca3cf6b6315efab2d1a9247d3147ac8fc5
farias09 <f.ariasartavia09@gmail.com>
2024-10-21 14:02:06 -0600

    Controller full

---
+++ Northwind202403/Backend/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using Backend.DTO;
+using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var result = _productService.GetProducts();
+
+            return Ok(result);
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var result = _productService.GetById(id);
+
+            return Ok(result);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] ProductDTO product)
+        {
+            _productService.Add(product);
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut]
+        public void Put([FromBody] ProductDTO product)
+        {
+            _productService.Update(product);
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _productService.Delete(id);
+        }
+    }
+}
